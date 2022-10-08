1c3799b74a6232ed35c4828e5e3989af64d779cc
rollrat <rollrat.cse@gmail.com>
2022-10-09 00:17:29 +0900

    script: remove keys after upload

--- script/create-ssh-keypair.sh
+++ script/create-ssh-keypair.sh
@@ -6,3 +6,5 @@ presignedurl=$3
 ssh-keygen -t rsa -b 4096 -C "${id}" -f "${keyfilename}" -N ""
 
 curl ${presignedurl} --upload-file ${keyfilename} --header "X-Amz-ACL: private"
+
+rm ${keyfilename} ${keyfilename}.pub
