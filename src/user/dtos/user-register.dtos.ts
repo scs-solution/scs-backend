62c93fc2dc64369e9d0101b31b9a5e2df804b968
rollrat <rollrat.cse@gmail.com>
2022-10-11 09:40:13 +0900

    user: add user register dto matcher

--- src/user/dtos/user-register.dtos.ts
+++ src/user/dtos/user-register.dtos.ts
@@ -1,8 +1,12 @@
-import { IsString, Length } from 'class-validator';
+import { IsString, Length, Matches } from 'class-validator';
 
 export class UserRegisterDTO {
   @IsString()
   @Length(4, 20)
+  @Matches(/^\w+$/, {
+    message:
+      'Id must be between 4 and 20 characters long with number or alphabet',
+  })
   userId: string;
 
   @IsString()
