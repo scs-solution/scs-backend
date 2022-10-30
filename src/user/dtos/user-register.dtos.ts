d4839da338fd850271c1ca3bd6b7fda8a123bead
rollrat <rollrat.cse@gmail.com>
2022-10-30 23:58:29 +0900

    user: allow ! special ch

--- src/user/dtos/user-register.dtos.ts
+++ src/user/dtos/user-register.dtos.ts
@@ -9,7 +9,7 @@ export class UserRegisterDTO {
   userId: string;
 
   @IsString()
-  @Matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).{6,64}$/, {
+  @Matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=]).{6,64}$/, {
     message:
       'Password must be between 6 and 64 characters long with 1 special character and capital character each',
   })
